b1e06ef9cfeb330fe76f328a658e743fb9bc4977
Rachel Tran <cahweb@ucf.edu>
2020-01-06 11:05:28 -0500

    Commented out previous styles.

--- common-events-2020.php
+++ common-events-2020.php
@@ -8,7 +8,7 @@
     License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-wp_enqueue_style('events-styles', get_stylesheet_directory_uri() . '/styles/events.css');
+// wp_enqueue_style('events-styles', get_stylesheet_directory_uri() . '/styles/events.css');
 
 // Essentially the function's main, except this is the actual main.
 include "functions/events.php";
